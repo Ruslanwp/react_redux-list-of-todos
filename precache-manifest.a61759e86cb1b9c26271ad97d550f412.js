d3aaeec0ffcee65d12f40720dbce698af3bfdea4
Test <test@Ruslan.local>
2021-04-26 17:46:59 +0300

    Updates

---
+++ precache-manifest.a61759e86cb1b9c26271ad97d550f412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9d905c6054a62a967266e8fba18157",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ca2f4ce10a9e2afc4cf",
+    "url": "./static/css/main.e73d41c8.chunk.css"
+  },
+  {
+    "revision": "9274dd0157789d298ce7",
+    "url": "./static/js/2.28696628.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.28696628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca2f4ce10a9e2afc4cf",
+    "url": "./static/js/main.de8b2c56.chunk.js"
+  },
+  {
+    "revision": "6e2c239f7a333241bc85",
+    "url": "./static/js/runtime-main.cb0ead06.js"
+  }
+]);
